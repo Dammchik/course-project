c031204ccc4e853f35f04d741fb208eac9a29c0f
Denis Damm <dvdamm@edu.hse.ru>
2024-03-08 22:26:27 +0300

    формочка с подтвержением и благодарностью

--- BarbershopWebApplication/Controllers/HomeController.cs
+++ BarbershopWebApplication/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using BarbershopWebApplication.Models;
 using Microsoft.AspNetCore.Mvc;
+
 namespace BarbershopWebApplication.Controllers
 {
     public class HomeController : Controller
@@ -11,9 +12,15 @@ namespace BarbershopWebApplication.Controllers
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
         }
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpFormPost(BarbershopRecordings barbershopRecordings) { 
+            Repository.AddRecordings(barbershopRecordings);
+            return View("Thanks", barbershopRecordings);
         }
     }
 }
---
+++ BarbershopWebApplication/Models/Repository.cs
@@ -0,0 +1,13 @@
+﻿namespace BarbershopWebApplication.Models
+{
+    public class Repository
+    {
+        private static List<BarbershopRecordings> _records = new List<BarbershopRecordings>();
+        public static IEnumerable<BarbershopRecordings> Recordings { get { return _records; } }
+        
+        public static void AddRecordings(BarbershopRecordings recordings)
+        {
+            _records.Add(recordings);   
+        }
+    }
+}
