858ce1b8b2a317c8dc674cf7ae0cdf746e3f74ff
Denis Damm <dvdamm@edu.hse.ru>
2024-04-01 01:06:38 +0300

    скорректирован композер и оформлено представление услуги

--- BarbershopWebApplication/Models/BarbershopContext.cs
+++ BarbershopWebApplication/Models/BarbershopContext.cs
@@ -6,9 +6,9 @@ namespace BarbershopWebApplication.Models
     {
         public DbSet<Barber> Barbers { get; set; }
         public DbSet<Service> Services { get; set; }
-        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Recording> Recordings { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=barbeshopdb;Username=dammchik;Password=123");
+            => optionsBuilder.UseNpgsql("Host=localhost;Port=15432;Database=barbershopdb;Username=postgres;Password=postgres");
     }
 }
---
+++ BarbershopWebApplication/Models/Recording.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarbershopWebApplication.Models
+{
+    public class Recording
+    {
+        public int RecordingId { get; set; }
+        public int BarberId { get; set; }
+        public Barber Barber { get; set; }
+        public int ServiceId { get; set; }
+        public Service Service { get; set; }
+        public DateTime Time { get; set; }
+            
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter email address")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you`ll attend")]
+        public bool? WillAttend { get; set; }
+        // Дополнительные свойства
+    }
+}
